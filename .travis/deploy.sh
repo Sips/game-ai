8d2e95aeaf1635f744d9e5d1442dd4984c099112
Dean de Bree <ddebree@gmail.com>
2017-05-01 11:29:37 +0200

    Install keys before deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -21,6 +21,10 @@ fi
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
+
+    openssl aes-256-cbc -K $encrypted_a65b4a5a53ac_key -iv $encrypted_a65b4a5a53ac_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+    gpg --fast-import .travis/codesigning.asc
+
     mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
